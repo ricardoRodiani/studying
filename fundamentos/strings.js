5283f2ac9cd7ce4db939f309bee30edb4584d306
ricardoRodiani <ricardo.rodiani@gmail.com>
2020-10-19 00:06:08 -0300

    Update strings.js

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -1,7 +1,6 @@
 const escola = "Cod3r"
 
 console.log(escola.charAt(4))
-<<<<<<< HEAD
 console.log(escola.charAt(5))
 console.log(escola.charCodeAt(3))
 console.log(escola.indexOf('3'))
@@ -14,6 +13,3 @@ console.log('Escola ' + escola + "!")
 console.log(escola.replace(3 , 'e'))
 
 console.log('Ana,Maria,Pedro'.split(','))
-=======
-console.log(escola.charAt(5))
->>>>>>> 68e25333b9da2cc59d4d6448f87dd8b60e1b1469
